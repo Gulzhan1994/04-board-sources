dfc4386ee65b7393d13b3c51c23d7a3ea159d231
Gulzhan1994 <gulzhan0404@gmail.com>
2021-10-12 17:17:38 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const board = document.querySelector('#board')
+const SQUARES_NUMBER = 400;
+const colors = ['#D30AEB', '#EB0A99', '#EB460A', '#EBE90A', '#36EB0A', '#0A3DEB']
+
+for(let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', setColor)
+
+    square.addEventListener('mouseleave', removeColor)
+
+    board.append(square)
+}
+
+function setColor(event) {
+    const element = event.target
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(event) {
+    const element = event.target
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
